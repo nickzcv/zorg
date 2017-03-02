248e5d8e2c475531e04f770440d9006f170556d4
Nick Z <nickzcv@gmail.com>
2017-03-02 23:07:05 +0300

    no message

--- app/js/app.js
+++ app/js/app.js
@@ -70,9 +70,6 @@ $(function() {
 });
 
 
-// This example displays an address form, using the autocomplete feature
-// of the Google Places API to help users fill in the information.
-
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">

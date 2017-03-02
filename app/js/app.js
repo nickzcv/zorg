7da81803361f98149765cdb65b3ef3dad195025b
Mikalai Zaitsau <mzaitsau@us.ibm.com>
2017-03-02 17:12:26 +0300

    remove dropdowns

--- app/js/app.js
+++ app/js/app.js
@@ -13,58 +13,6 @@ $(function() {
     $('.navigation').toggleClass('hidden');
   });
 
-  /*
-   * Drop-down navigation handler
-   *
-   */
-  var timer;
-  $('.sub-nav-trigger').on({
-    mouseenter: function(){
-      var self = this;
-      clearTimeout(timer);
-      timer = setTimeout(function(){
-        $(self).next().fadeIn();
-      }, 100)
-    },
-    mouseleave: function(){
-      var self = this;
-      setTimeout(function(){
-        if (!$(self).next().is(":hover")){
-          $(self).next().fadeOut();
-        }
-      }, 300);
-    }
-  });
-
-  $('.sub-nav').on('mouseleave', function() {
-    $(this).fadeOut();
-  });
-
-  /*
-   * Cart popup handler
-   *
-   */
-  $('.cart').on({
-    mouseenter: function(){
-      var self = this;
-      clearTimeout(timer);
-      timer = setTimeout(function(){
-        $(self).find('.cart-popup').fadeIn();
-      }, 100)
-    },
-    mouseleave: function(){
-      var self = this;
-      setTimeout(function(){
-        if (!$(self).find('.cart-popup').is(":hover")){
-          $(self).find('.cart-popup').fadeOut();
-        }
-      }, 300);
-    }
-  });
-
-  $('.cart-popup').on('mouseleave', function() {
-    $(this).fadeOut();
-  });
 
   /*
    * Shop category navigation handlers

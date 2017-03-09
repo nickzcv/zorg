b9cf7bf9ebc38b425a398320aabf8d816f65b1b2
Mikalai Zaitsau <mzaitsau@us.ibm.com>
2017-03-09 17:39:46 +0300

    update filter section

--- app/js/main.js
+++ app/js/main.js
@@ -7,8 +7,6 @@ $(function() {
    *
    */
   $('.hamburger').on('click', function() {
-    // hide cart if opened
-    $('.cart-popup').hide();
     $(this).toggleClass('closeBtn');
     $('.navigation').toggleClass('hidden');
   });
@@ -18,54 +16,15 @@ $(function() {
    * Shop category navigation handlers
    *
    */
-  $('.mobile-arrow').on('touchstart', function() {
-    var parentNode = $(this).parent();
-
-    if (parentNode.hasClass('opened')) {
-      parentNode.removeClass('opened');
-    } else {
-      parentNode.addClass('opened');
-    }
+  $('.filter-trigger').on('click', function() {
+    $(this).toggleClass('minusBtn');
+    $('.filter-body').toggle(200);
   });
 
-  // Touch somewhere inside category navigation
-  $('.categories li').on('touchstart', function(event) {
-    event.preventDefault();
-    // if touched arrow - do nothing
-    if ($(event.target).closest('li').hasClass('mobile-arrow')) {
-      return;
-    }
-    // if nav already was opened
-    if ($(event.target).closest('.categories').hasClass('opened')) {
-      // TODO: check if we have cheld section
-      selectItem(event.target);
-    } else {
-      $('.categories').addClass('opened');
-      selectItem(event.target);
-    }
-  });
 
-  // Select item handler
-  function selectItem(target){
-    var $currentItem =  $(target).closest('a');
-    // if element already was selected
-    // then open current link
-    if ($currentItem.hasClass('selected')) {
-      location.href = $currentItem.attr('href');
-      return;
-    }
-    $currentItem.addClass('selected');
-    // Clear other selections
-    $currentItem.parent().siblings().find('a').removeClass('selected');
-  }
 
-  // Hide navigation when touch somewhere outside navigation
-  $(document).on('touchstart', function(event) {
-    if (!$(event.target).closest('.categories').length) {
-      $('.categories').removeClass('opened');
-      $('.categories li a').removeClass('selected');
-    }
-  });
+
+
 
 });
 
